47b330e53135490800fa2bff1bde5ea12b623a73
GithubAkvns <108998560+GithubAkvns@users.noreply.github.com>
2022-07-10 00:51:39 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		// find the factorial
+				
+		int i=5;
+		int fact=1;
+		
+		//loop through 
+		for (int j = 1; i <=i; i++) {
+            fact = fact*i;
+		}
+              System.out.println(fact);
+		
+
+	}
+
+}
---
+++ OddorEven.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class OddorEven {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		
+		if(a%2 == 0  ) {
+		System.out.println("It is even");	
+		
+		}
+		else {
+			System.out.println("It is odd");
+		}
+		
+
+	}
+
+}
